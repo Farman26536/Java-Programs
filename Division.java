cc3096f03407a44b04fb41e1868680e46967a535
Farman23536 <farmanullah26f@gmail.com>
2026-07-16 11:04:31 +0530

    sample push

--- Division.java
+++ Division.java
@@ -1,4 +1,4 @@
-public class Division {
+public class Main {
     public int divide(int dividend, int divisor) {
           if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;

be5cfd7223b97cf0de99772cbc692105ef57326f
Farman23536 <farmanullah26f@gmail.com>
2026-07-20 10:07:01 +0530

    sample push

---
+++ Instance.java
@@ -0,0 +1,23 @@
+public class Instance
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    // If the object is null/undefined or the classFunction isn't a valid constructor/function
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false;
+    }
+
+    // Traverse up the prototype chain
+    let currPrototype = Object.getPrototypeOf(obj);
+    while (currPrototype !== null) {
+        if (currPrototype === classFunction.prototype) {
+            return true;
+        }
+        currPrototype = Object.getPrototypeOf(currPrototype);
+    }
+
+    return false;
+};
